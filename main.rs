c6f1775bd90fc0523f87b9e018d9b6c0f7596dba
Benjamin Adamson <adamson.benjamin@gmail.com>
2014-01-25 12:35:17 -0800

    Have a moving ball on the screen.

--- main.rs
+++ main.rs
@@ -2,40 +2,106 @@ extern mod native;  // TO start a native thread
 extern mod rsfml;   // Multimedia library
 
 use rsfml::window::{ContextSettings, VideoMode, event, keyboard, Close};
-use rsfml::graphics::{RenderWindow, Color, Texture, Sprite, RenderStates,
-    RenderTexture, IntRect};
+use rsfml::graphics::{RenderWindow, Color, Texture, Sprite, IntRect, CircleShape};
 use rsfml::system::vector2::Vector2f;
-use rsfml::traits::drawable::Drawable;
 
 use std::hashmap::HashMap;
 use std::num::FromPrimitive;
-//use std::option;
+use std::rand::{task_rng, Rng};
 
 // Window Defaults
-static window_width:  uint = 1024;
-static window_height: uint = 768;
-static pixels:        uint = 32;
-static paddle_width:  i32 = 20;  // tyeps are weird due to RSFML binding.
-static paddle_height: i32 = 50;  // tyeps are weird due to RSFML binding.
+static WINDOW_WIDTH:  uint = 1024;
+static WINDOW_HEIGHT: uint = 768;
+static PIXEL_COUNT:   uint = 32;
+static PADDLE_WIDTH:  i32 = 20;  // tyeps are weird due to RSFML binding.
+static PADDLE_HEIGHT: i32 = 50;  // tyeps are weird due to RSFML binding.
 
 // Game Option Defaults
-static paddle_padding:     f32 = 30.;
-static lhs_start_pos_x:    f32 = 0. + paddle_padding;
-static rhs_start_pos_x:    f32 = (window_width as f32) - paddle_padding
-  - (paddle_width as f32);
-static bottom_start_pos_y: f32 = (window_height as f32) - paddle_padding
-  - (paddle_height as f32);
+static PADDLE_PADDING:     f32 = 30.;
+static LHS_START_POS_X:    f32 = 0. + PADDLE_PADDING;
+static RHS_START_POS_X:    f32 = (WINDOW_WIDTH as f32) - PADDLE_PADDING - (PADDLE_WIDTH as f32);
+static bottom_start_pos_y: f32 = (WINDOW_HEIGHT as f32) - PADDLE_PADDING - (PADDLE_HEIGHT as f32);
 
-static paddle_velocity: f32 = 5.;
-static up_vector:   Vector2f  = Vector2f { x:  0., y:  1. * paddle_velocity };
-static down_vector: Vector2f  = Vector2f { x:  0., y: -1. * paddle_velocity };
+static PADDLE_VELOCITY: f32 = 5.;
+static UP_VECTOR:       Vector2f  = Vector2f { x:  0., y:  1. * PADDLE_VELOCITY };
+static DOWN_VECTOR:     Vector2f  = Vector2f { x:  0., y: -1. * PADDLE_VELOCITY };
 
-static start_positions: [Vector2f, ..4] = [
-  Vector2f { x: lhs_start_pos_x, y: 0. + paddle_padding }, // Player 1
-  Vector2f { x: rhs_start_pos_x, y: 0. + paddle_padding }, // Player 2
-  Vector2f { x: lhs_start_pos_x, y: bottom_start_pos_y },  // Player 3
-  Vector2f { x: rhs_start_pos_x, y: bottom_start_pos_y },  // Player 4
+static BALL_RADIUS:            f32 = 10.;
+static BALL_OUTLINE_THICKNESS: f32 = 3.;
+static BALL_INITIAL_POSITION:  Vector2f = Vector2f {
+    x: (WINDOW_WIDTH as f32) / 2.,
+    y: (WINDOW_HEIGHT as f32)  / 2.
+};
+static BALL_VELOCITY:      f32 = 5.;
+static BALL_FILL_COLOR:    Color = Color { red: 255, green: 0, blue: 0, alpha: 255 };
+static BALL_OUTLINE_COLOR: Color = Color { red: 255, green: 0, blue: 255, alpha: 255 };
+
+static START_POSITIONS: [Vector2f, ..4] = [
+  Vector2f { x: LHS_START_POS_X, y: 0. + PADDLE_PADDING }, // Player 1
+  Vector2f { x: RHS_START_POS_X, y: 0. + PADDLE_PADDING }, // Player 2
+  Vector2f { x: LHS_START_POS_X, y: bottom_start_pos_y },  // Player 3
+  Vector2f { x: RHS_START_POS_X, y: bottom_start_pos_y },  // Player 4
 ];
+
+#[deriving(Eq, Clone, IterBytes, FromPrimitive)]
+enum PlayerId {
+  bluepaddle,
+  greenpaddle
+}  // enum PlayerId
+
+struct Paddle<'r> {
+  sprite: Sprite<'r>
+}  // struct Paddle
+
+struct Ball<'r> {
+  drawable: CircleShape<'r>,
+  velocity: Vector2f
+}  // struct Ball
+
+struct PongGameState<'r> {
+  window:  &'r mut RenderWindow,
+  paddles: ~[Paddle<'r>],
+  player_id: PlayerId,
+  ball: Ball<'r>,
+  keys: ~[keyboard::Key]
+}  // struct PongGameState
+
+impl<'r> PongGameState<'r> {
+  fn new_default(paddles_param: ~[Paddle<'r>], window_param: &'r mut RenderWindow,
+      player_id_param: PlayerId, ball_param: Ball<'r>) -> PongGameState<'r> {
+    assert!((player_id_param as uint) < paddles_param.len());
+    return PongGameState {
+        keys: ~[],
+        paddles: paddles_param,
+        player_id: player_id_param,
+        window: window_param,
+        ball: ball_param
+    };
+  }  // fn new_default()
+  
+  // Construct a new state from an existing one.
+  fn from_previous(prev: PongGameState<'r>) -> PongGameState<'r> {
+    let mut state = prev;
+    let player_index: uint = state.player_id as uint;
+    { 
+      let player_paddle = &mut state.paddles[player_index];
+      for key in state.keys.iter() {
+        match *key {
+          keyboard::Escape => state.window.close(),
+          keyboard::K => { player_paddle.sprite.move(&UP_VECTOR); },
+          keyboard::J => { player_paddle.sprite.move(&DOWN_VECTOR); },
+          _ => {}
+        }
+      }
+    }
+    {
+      let velocity = state.ball.velocity * BALL_VELOCITY;
+      state.ball.drawable.move(&velocity);
+    }
+    state.keys.clear();
+    return state;
+  }  // fn from_previous()
+}  // impl PongGameState
 
 // OSX Prevents creating a window on the main thread, so start up a new thread
 // and launch the window.
@@ -49,7 +115,7 @@ fn start(argc: int, argv: **u8) -> int {
 fn create_window() -> (RenderWindow, Color) {
   let title       = "RSFML Pong - Rust";
   let settings    = ContextSettings::default();
-  let video_mode  = VideoMode::new_init(window_width, window_height, pixels);
+  let video_mode  = VideoMode::new_init(WINDOW_WIDTH, WINDOW_HEIGHT, PIXEL_COUNT);
   let clear_color = Color::new_RGB(255, 255, 255);
 
   let window = match RenderWindow::new(video_mode, title, Close, &settings) {
@@ -59,93 +125,24 @@ fn create_window() -> (RenderWindow, Color) {
   return (window, clear_color);
 }  // fn create_window()
 
-#[deriving(Eq, Clone, IterBytes, FromPrimitive)]
-enum PlayerId {
-  bluepaddle,
-  greenpaddle
-}  // enum PlayerId
-
-struct Paddle<'r> {
-  sprite: Sprite<'r>
-}  // struct Paddle
-
-impl<'r> Drawable for Paddle<'r> {
-  fn draw_in_render_window(&self, render_window : &RenderWindow) -> () {
-    render_window.draw_sprite(&self.sprite)
-  }
-
-  fn draw_in_render_window_rs(&self, render_window : &RenderWindow,
-      render_states : &mut RenderStates) -> () {
-    render_window.draw_sprite_rs(&self.sprite, render_states)
-  }
-
-  fn draw_in_render_texture(&self, render_texture : &RenderTexture) -> () {
-    render_texture.draw_sprite(&self.sprite)
-  }
-
-  fn draw_in_render_texture_rs(&self, render_texture : &RenderTexture,
-      render_states : &mut RenderStates) -> () {
-    render_texture.draw_sprite_rs(&self.sprite, render_states)
-  }
-} // impl Drawable for Paddle
-
-struct PongGameState<'r> {
-  window:  &'r mut RenderWindow,
-  paddles: ~[Paddle<'r>],
-  player_id: PlayerId,
-  //controlled: &'r Paddle<'r>,
-  keys: ~[keyboard::Key]
-}  // struct PongGameState
-
-impl<'r> PongGameState<'r> {
-  fn new_default(paddles_param: ~[Paddle<'r>],
-      window_param: &'r mut RenderWindow, player_id_param: PlayerId)
-      -> PongGameState<'r> {
-    assert!((player_id_param as uint) < paddles_param.len());
-    return PongGameState { keys: ~[], paddles: paddles_param,
-      player_id: player_id_param, window: window_param };
-  }  // fn new_default()
-  
-  // Construct a new state from an existing one.
-  fn from_previous(prev: PongGameState<'r>) -> PongGameState<'r> {
-    let mut state = prev;
-    let player_index: uint = state.player_id as uint;
-    { 
-      let player_paddle = &mut state.paddles[player_index];
-      for key in state.keys.iter() {
-        match *key {
-          keyboard::Escape => state.window.close(),
-          keyboard::K => { player_paddle.sprite.move(&up_vector); },
-          keyboard::J => { player_paddle.sprite.move(&down_vector); },
-          _ => {}
-        }
+// Create a ball that's initialized with the values we declare statically.
+fn create_ball() -> Ball {
+  let mut rng = task_rng();
+  let mut ball = Ball {
+      drawable: CircleShape::new().expect("Could not instantiate ball"),
+      velocity: Vector2f {
+          x: rng.gen_range::<f32>(-1., 1.),
+          y: rng.gen_range::<f32>(-1., 1.)
       }
-    }
-    state.keys.clear();
-    return state;
-  }  // fn from_previous()
-}  // impl PongGameState
+  };
+  ball.drawable.set_radius(BALL_RADIUS);
+  ball.drawable.set_outline_thickness(BALL_OUTLINE_THICKNESS);
+  ball.drawable.set_fill_color(&BALL_FILL_COLOR);
+  ball.drawable.set_outline_color(&BALL_OUTLINE_COLOR);
+  ball.drawable.set_position(&BALL_INITIAL_POSITION);
 
-// Loads the different textures as pairs with their corresponding PlayerId
-fn load_assets() -> HashMap<PlayerId, Texture> {
-  let dir               = "./assets/";
-  let blue_paddle_path  = dir + "blue-paddle.png";
-  let green_paddle_path = dir + "green-paddle.png";
-  let error_prefix      = "Could not load asset: ";
-
-  let texture_rect = IntRect { left: 0, top: 0, width: paddle_width,
-    height: paddle_height };
-  let blue_paddle_texture = Texture::new_from_file_with_rect(
-    blue_paddle_path, &texture_rect).expect(error_prefix + blue_paddle_path);
-
-  let green_paddle_texture = Texture::new_from_file_with_rect(
-    green_paddle_path, &texture_rect).expect(error_prefix + green_paddle_path);
-
-  let mut hs = HashMap::new();
-  hs.insert(bluepaddle, blue_paddle_texture);
-  hs.insert(greenpaddle, green_paddle_texture);
-  return hs;
-} // fn load_assets
+  return ball;
+}  // fn create_ball()
 
 // Constructs (PlayerId, Sprite) corresponding 1:1 with pairs from the input
 // HashMap (PlayerId, Texture). The sprites returned have been construted with
@@ -167,6 +164,47 @@ fn create_sprites<'r>(assets: &'r HashMap<PlayerId, Texture>)
   return sprite_map;
 }  // fn create_sprites()
 
+// Constructs a vector of Padddles, one paddle for each item in the sprites hashmap
+// parameter. The Paddles returned own the sprite. The interesting bit of implementation
+// is zipping the sprites with the START_POSITIONS vector. This creates an iterator we
+// can iterate over, yielding pairs of ((PlayerId, Sprite), Vector2f).
+fn create_paddles(sprites: HashMap<PlayerId, Sprite>) -> ~[Paddle] {
+  // Zip the iterators together, so they can be iterated together. :)
+  let zipped = sprites.iter().zip(START_POSITIONS.iter());
+
+  return zipped
+    .map(|((_, sprite_item), start_pos_item)| {
+      let error_msg = "Error cloning sprite.";
+      let mut paddle = Paddle {
+        // todo: do I really have to clone? I just want to move the sprite's ...
+        sprite: sprite_item.clone().expect(error_msg)
+      };
+      paddle.sprite.set_position(start_pos_item);
+      return paddle; 
+    }).collect();
+}  // fn create_paddles()
+
+// Loads the different textures as pairs with their corresponding PlayerId
+fn load_assets() -> HashMap<PlayerId, Texture> {
+  let dir               = "./assets/";
+  let blue_paddle_path  = dir + "blue-paddle.png";
+  let green_paddle_path = dir + "green-paddle.png";
+  let error_prefix      = "Could not load asset: ";
+
+  let texture_rect = IntRect { left: 0, top: 0, width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT };
+  let blue_paddle_texture = Texture::new_from_file_with_rect(
+    blue_paddle_path, &texture_rect).expect(error_prefix + blue_paddle_path);
+
+  let green_paddle_texture = Texture::new_from_file_with_rect(
+    green_paddle_path, &texture_rect).expect(error_prefix + green_paddle_path);
+
+  let mut hs = HashMap::new();
+  hs.insert(bluepaddle, blue_paddle_texture);
+  hs.insert(greenpaddle, green_paddle_texture);
+  return hs;
+} // fn load_assets
+
 // The scan fn below returns an iterator that will iterator over the sprite's'
   // in assets HashMap.
   // The initial state of scan() is the sprites iterator.
@@ -181,20 +219,7 @@ fn create_sprites<'r>(assets: &'r HashMap<PlayerId, Texture>)
       return sprite.clone();
     }); */
 
-fn create_paddles(sprites: HashMap<PlayerId, Sprite>) -> ~[Paddle] {
-  // Zip the iterators together, so they can be iterated together. :)
-  let zipped = sprites.iter().zip(start_positions.iter());
 
-  return zipped
-    .map(|((_, sprite_item), start_pos_item)| {
-      let error_msg = "Error cloning sprite.";
-      let mut paddle = Paddle {
-        sprite: sprite_item.clone().expect(error_msg)
-      };
-      paddle.sprite.set_position(start_pos_item);
-      return paddle; 
-    }).collect();
-}  // fn create_paddles()
 
 // Loop forever polling events from the window, until there are no more events.
 // When there are no more events, break out of the loop.
@@ -218,19 +243,22 @@ fn main() {
   let sprites = create_sprites(&assets);
   let paddles = create_paddles(sprites);
 
+  let ball = create_ball();
+
   // Each state needs a reference to the items it needs to update..
   // For example, it needs a reference to the paddle array.
   let player_id = FromPrimitive::from_int(0).expect("PlayerId");
-  let mut state = PongGameState::new_default(paddles, &mut window, player_id);
+  let mut state = PongGameState::new_default(paddles, &mut window, player_id, ball);
 
-  // when I press the 'hjkl' keys, move the first paddle..
+  // when I press the 'j | k' keys, move the first paddle..
   while state.window.is_open() {
     state.window.clear(&clear_color);
     state = loop_events(state); 
     // update_state
 
     for paddle in state.paddles.iter() {
-      state.window.draw(paddle);
+      state.window.draw(&state.ball.drawable);
+      state.window.draw(&paddle.sprite);
     }
     state.window.display();
   }
